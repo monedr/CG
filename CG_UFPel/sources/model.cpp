015aad983fa202152d17c6fe96f6b97725a241b7
Simone Rutz <sdrutz@inf.ufpel.edu.br>
2016-04-29 15:03:20 -0300

    escala ok

--- CG_UFPel/sources/model.cpp
+++ CG_UFPel/sources/model.cpp
@@ -44,6 +44,8 @@ void model::setTransformation() {
 	//ModelMatrix = glm::rotate(glm::mat4(1.0), glm::degrees(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	//translação ok
 	//ModelMatrix = glm::translate(glm::mat4(1.0), glm::vec3(0.5f, 0.5f, 0.5f));
+	// escala ok
+	//ModelMatrix = glm::scale(2.0f, 2.0f, 2.0f);
 }
 
 glm::mat4 model::getModelMatrix(){
